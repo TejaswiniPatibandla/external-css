4f7ac2f7370a41ee2e72e9892bf7a5f28848eb8a
TejaswiniPatibandla <106171965+TejaswiniPatibandla@users.noreply.github.com>
2022-06-10 08:59:43 +0530

    basic.js

---
+++ basic.js
@@ -0,0 +1,11 @@
+
+<html>
+  <head>
+    <title>welcome to css</title>
+      <body>
+        <script>
+          alert("displays an alert box");
+          document.write("this is basic javascritpt");
+        </script>
+    </body>
+    </html>
